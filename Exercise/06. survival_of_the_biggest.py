9dd97f6c7f3f3e8a9f00f49f7bd0ecafd764dadf
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-28 23:16:34 +0200

    0666

--- Exercise/06. survival_of_the_biggest.py
+++ Exercise/06. survival_of_the_biggest.py
@@ -1,6 +1,9 @@
-n = int(input())
+numbers = input()
 magic_number = int(input())
-
-
-for number in range(n):
+current_number = 0
+for number in numbers:
+    if current_number < numbers:
+        current_number.append(number)
+            
     
+print(current_number)    
