1b9d77a72ec02c5149008441934e39b31e803d28
me_madness <p_rangel@abv.bg>
2023-09-29 18:29:53 +0200

    02. Are done

--- Exercise/02. multiples_list.py
+++ Exercise/02. multiples_list.py
@@ -1,3 +1,5 @@
+# First option
+
 multiply_number = int(input())
 range_number = int(input())
 
@@ -8,3 +10,15 @@ for num in range(1,range_number + 1):
     list_digit.append(current_number)
     
 print(list_digit)    
+
+# Second option
+
+multiply_number = int(input())
+range_number = int(input())
+
+list_digit = []
+
+for num in range(1,range_number + 1):
+    current_number = list_digit.append(num * multiply_number)
+    
+print(list_digit)
