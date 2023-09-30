8cc2ee28d89e1cf081f1a5d43bece96194c55b36
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-30 21:32:34 +0200

    strat removing

--- Exercise/03. football_cards.py
+++ Exercise/03. football_cards.py
@@ -1,9 +1,9 @@
 team_a = ["A-1", "A-2", "A-3", "A-4", "A-5", "A-6", "A-7", "A-8", "A-9", "A-10", "A-11"]
 team_b = ["B-1", "B-2", "B-3", "B-4", "B-5", "B-6", "B-7", "B-8", "B-9", "B-10", "B-11"]
-card_input = input()
+card_input = input().split(' ')
 red_card = False
 
-empty_list = [] 
+# empty_list = [] 
 
 for card in card_input:
     if "A-" in card:
