c2d49d44550bce3e4729e3f33517ea12f24d09d2
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-02 22:36:44 +0200

    05. it's all most finish man

--- Exercise/05. faro_shuffle.py
+++ Exercise/05. faro_shuffle.py
@@ -5,4 +5,7 @@ for shuffle in range(count_of_shuffles):
     left_part = deck_of_card[0:middle_of_deck]
     right_part = deck_of_card[middle_of_deck:]
     deck_after_shuffling = []
-    for card
+    for card_index in range(len(left_part)):
+        deck_after_shuffling.append(left_part[card_index])
+        deck_after_shuffling.append(right_part[card_index])
+    deck_of_card = deck_after_shuffling    
