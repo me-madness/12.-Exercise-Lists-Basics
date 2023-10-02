3c197ba38eee1344bfb24d052e970f988f04bc5e
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-02 22:10:47 +0200

    04. done

--- Exercise/04. nummber_beggards.py
+++ Exercise/04. nummber_beggards.py
@@ -5,9 +5,11 @@ for current_money in line_of_input:
     money_integers.append(int(current_money))
 final_sum = []
 start_index = 0
-while start_index < money_integers:
+while start_index < count_of_beggars:
     current_beggar_sum = 0
     for current_index in range(start_index, len(money_integers),count_of_beggars):
         current_beggar_sum += money_integers[current_index]
     final_sum.append(current_beggar_sum)
     start_index += 1
+    
+print(final_sum)    
