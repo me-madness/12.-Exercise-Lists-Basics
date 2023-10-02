2aeaa4fdebab3d55e2dcf2c7ea35c31bc2500fef
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-02 22:31:40 +0200

    go go forest

--- Exercise/05. faro_shuffle.py
+++ Exercise/05. faro_shuffle.py
@@ -1,2 +1,8 @@
 deck_of_card = input().split()
 count_of_shuffles = int(input())
+for shuffle in range(count_of_shuffles):
+    middle_of_deck = len(deck_of_card) // 2
+    left_part = deck_of_card[0:middle_of_deck]
+    right_part = deck_of_card[middle_of_deck:]
+    deck_after_shuffling = []
+    for card
