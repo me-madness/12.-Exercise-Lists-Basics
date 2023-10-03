642fdc5d453eaca005170a5576512bff03f4a320
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-03 22:38:19 +0200

    adada

--- Exercise/06. survival_of_the_biggest.py
+++ Exercise/06. survival_of_the_biggest.py
@@ -2,9 +2,10 @@ numbers = input()
 magic_number = int(input())
 current_number = []
 for number in numbers:
+    current_input = number
     if current_input < number:
-        current_input = number
+        current_number.append(number)
     
             
     
-print(current_input)    
+print(current_number)    
